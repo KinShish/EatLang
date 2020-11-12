ea71245d1c96ba62ead4825400e476d951eafcd4
Прохач Дмитрий <dimas999000pro@mail.ru>
2020-11-12 10:40:32 +0300

    поменял сервер

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 const settings={
   server: "https://01taxi.ru/vtr/",
   //server: "http://192.168.3.123:3000/",
-  serverChat:"https://01taxi.ru"
+  serverChat:"http://192.168.3.123:3000"
 }
 module.exports=settings;
