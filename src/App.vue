a3837d977f08cfcbb1fe0ded7d92c8e64a07a291
Прохач Дмитрий <dimas999000pro@mail.ru>
2020-11-27 20:27:51 +0300

    текст в речь и обратно

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template lang="pug">
 	div
-		h1 {{text}}
+		button.col-12(@click="sayEn") en
+		button.col-12(@click="sayRu") ru
+		input.col-12(v-model="text")
+		button.col-12(@click="testmic") testmic
+		.row
+			button.col-6(@click="startListing") start
+			button.col-6(@click="stopListing") stop
+		span.col-12 {{arrText}}
 </template>
 
 <script>
 	export default {
 		data(){
 			return{
-				text:'asdasd'
+				text:'',
+				arrText:[]
 			}
-		}
+		},
+		methods:{
+			sayRu(){
+				// eslint-disable-next-line no-undef
+				TTS
+					.speak({
+						text: this.text,
+						locale: 'ru-ru',
+						rate: 0.4,
+						pitch: 1.3,
+						cancel: true
+					})
+			},
+			sayEn(){
+				// eslint-disable-next-line no-undef
+				TTS
+					.speak({
+						text: this.text,
+						locale: 'en-US',
+						rate: 0.4,
+						pitch: 1.3,
+						cancel: true
+					})
+			},
+			test(type){
+				alert(type)
+			},
+			startListing(){
+				window.plugins.speechRecognition.startListening(
+					(res)=>{console.log('startListeningSuc',res);this.arrText=res;this.text=this.arrText[0]},
+					(res)=>{console.log('startListeningBad',res)},)
+			},
+			stopListing(){
+				window.plugins.speechRecognition.stopListening(
+					(res)=>{console.log('stopListeningSuc',res)},
+					(res)=>{console.log('stopListeningBad',res)},)
+			},
+			testmic(){
+				// eslint-disable-next-line no-undef
+				window.plugins.speechRecognition.requestPermission(
+					(requestPermissionSuc)=>{console.log('requestPermission',requestPermissionSuc)} ,
+					(requestPermissionErr)=>{console.log('requestPermission',requestPermissionErr)})
+			}
+		},
 	}
 </script>
 
